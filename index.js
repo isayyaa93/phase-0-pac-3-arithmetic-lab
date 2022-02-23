080b0c58a2054031aeabacc057ccb74048dbed06
Isaiah Kang <i.say.yaa@gmail.com>
2022-02-23 18:50:50 -0500

    commit tree 9bb55f2b36684d91d6ac3fff9830e638575d5ce9

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(n1, n2){
+    return n1 + n2;
+}
+
+function subtract(n1, n2){
+    return n1 - n2;
+}
+
+function multiply(n1, n2){
+    return n1 * n2;
+}
+
+function divide(n1, n2){
+    return n1/n2;
+}
+
+
+function increment(n){
+    return n++;
+}
+
+function decrement(n){
+    return n--;
+}
+
+console.log(increment(3));
+
+function makeInt(string){
+    return parseInt(string)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
